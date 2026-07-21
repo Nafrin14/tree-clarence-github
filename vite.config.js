6bfe8599d3576d1367bb443d8c35cae58b8268c0
Nafrin14 <nafrinnafrin49@gmail.com>
2026-07-21 22:54:26 +0530

    final change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/tree-clarence-github/",
   plugins: [react(), tailwindcss()],
 });
