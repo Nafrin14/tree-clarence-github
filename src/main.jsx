167c2fa9ad2da02d3f147295226e26c252664c12
Nafrin14 <nafrinnafrin49@gmail.com>
2026-07-24 23:45:53 +0530

    Fix router basename for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
-const rootElement = document.getElementById("root");
 
-if (!rootElement) {
-  throw new Error("Root element was not found.");
-}
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-createRoot(rootElement).render(
-  <BrowserRouter basename="/tree-clarence-github">
+
+root.render(
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
